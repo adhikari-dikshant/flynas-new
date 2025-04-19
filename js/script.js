9b10940d46d838a90260866cf48e1540a1374227
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2025-04-20 00:39:59 +0530

    splash screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 const lenis = new Lenis({
     autoRaf: true
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const splashScreen = document.querySelector('.splash-screen');
+    const splashVideo = splashScreen.querySelector('video');
+    splashVideo.addEventListener('ended', hideSplashScreen);
+    setTimeout(hideSplashScreen, 5000); // 5 seconds
+
+    function hideSplashScreen() {
+        splashScreen.classList.add('fade-out');
+        setTimeout(() => {
+            splashScreen.style.display = 'none';
+        }, 500);
+    }
+
+    document.body.style.overflow = 'hidden';
+
+    splashVideo.addEventListener('ended', function () {
+        document.body.style.overflow = '';
+    });
+});
